77e349a58d708e6c627c5296deec79f1ca51fb05
Daniel Kilders <dankil.dev@gmail.com>
2020-11-23 15:31:11 +0100

    Initial Commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Dani Kilders on 23.11.20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
